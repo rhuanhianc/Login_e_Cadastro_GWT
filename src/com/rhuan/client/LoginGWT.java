3278529639b92d9bac4eadf2de3aa3c31d4d3602
Rhuan S. Hianc <rhuanhianc123@gmail.com>
2021-07-13 22:55:05 -0300

    feito todo o design css

--- src/com/rhuan/client/LoginGWT.java
+++ src/com/rhuan/client/LoginGWT.java
@@ -4,7 +4,7 @@ import com.rhuan.client.cadastro.CadastroUsuarios;
 import com.rhuan.client.home.home;
 import com.rhuan.shared.FieldVerifier;
 
-import org.apache.jasper.compiler.ELNode.Root;
+
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
@@ -13,11 +13,17 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.dom.client.MouseDownEvent;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.PasswordTextBox;
@@ -34,50 +40,75 @@ public class LoginGWT implements EntryPoint {
 		return GWT.create(LoginService.class);
 		
 	}
-	private Label lblLogin = new Label("Usuario");
-	private Label lblSenha = new Label("Senha");
+	static DialogBox dbPanel= new DialogBox(){;
+    protected void beginDragging(MouseDownEvent event) {
+        event.preventDefault();
+    };
+};
+	private Label lblLogin = new Label("Usuario:");
+	private Label lblSenha = new Label("Senha:");
+	private Label lblLoginErrado = new Label("Usuario ou senha invalidos!");
+	private Label lblLoginBranco = new Label("Preencha todos os campos!");
 	
 	private TextBox txtLogin = new TextBox();
+	
 	private PasswordTextBox txtSenha = new PasswordTextBox();
 	
 	Button btnEntrar =new Button("Entrar");
 	Button btnCadastrar =new Button("Cadastrar");
 	
+	FlexTable flTable = new FlexTable();
 	VerticalPanel vPanel = new VerticalPanel();
-	static DialogBox dbPanel = new DialogBox();
+	Image image = new Image("images/usericon.png");
+	Timer t = new Timer() {
+	      @Override
+	      public void run() {
+	    	  lblLoginErrado.setVisible(false);
+	    	  lblLoginBranco.setVisible(false);
+	      }
+	};
 	public void onModuleLoad() {
 		
+		vPanel.add(image);
 		vPanel.add(lblLogin);
 		vPanel.add(txtLogin);
 		vPanel.add(lblSenha);
 		vPanel.add(txtSenha);
-		vPanel.add(btnEntrar);
-		vPanel.add(btnCadastrar);
+		vPanel.add(lblLoginErrado);
+		vPanel.add(lblLoginBranco);
+		lblLoginErrado.setStyleName("erro");
+		lblLoginBranco.setStyleName("erro");
+		flTable.setWidget(0, 0, btnCadastrar);
+		flTable.setWidget(0, 8, btnEntrar);
 		
-		
+		flTable.setCellPadding(5);
+		lblLoginErrado.setVisible(false);
+		lblLoginBranco.setVisible(false);
+		vPanel.add(flTable);
+		txtLogin.getElement().setPropertyString("placeholder", "Digite o nome de usuario");
+		txtSenha.getElement().setPropertyString("placeholder", "Digite a sua senha");
+	
 		btnEntrar.addClickHandler(new ClickHandler() {
 			
 			@Override
 			public void onClick(ClickEvent event) {
-				getServico().valLogin(txtLogin.getText(), txtSenha.getText(), callback);
-				dbPanel.hide();
-				RootPanel.get().clear();
+				
+					getServico().valLogin(txtLogin.getText(), txtSenha.getText(), callback);
+				
 			}
 		});
 		
 	
 		dbPanel.add(vPanel);
-		dbPanel.setText("Digite usuario e senha: ");
+		dbPanel.center();
 		dbPanel.setAnimationEnabled(true);
-	
-
 		
 	btnCadastrar.addClickHandler(new ClickHandler() {
 			
 			@Override
 			public void onClick(ClickEvent event) {
-				dbPanel.hide();
 				RootPanel.get().clear();
+				dbPanel.hide();
 				 new CadastroUsuarios();
 
 			}
@@ -85,6 +116,7 @@ public class LoginGWT implements EntryPoint {
 		
 		
 		RootPanel.get().add(dbPanel);
+		dbPanel.center();
 	}
 	
 	
@@ -100,15 +132,24 @@ public class LoginGWT implements EntryPoint {
 
 		@Override
 		public void onSuccess(String result) {
-			if(result.equals("valido")) 
-			
-			RootPanel.get().add((IsWidget) new home());
-			
-			
-			else
-				Window.alert("usuario ou senha invalida");
+			if (result.equals("valido")) {
+				RootPanel.get().clear();
+				dbPanel.hide();
+
+				RootPanel.get().add((IsWidget) new home());
+			}
+
+			else {
+				if (txtLogin.getText().isEmpty() || txtSenha.getText().isEmpty()) {
+					lblLoginBranco.setVisible(true);
+					t.schedule(5000);
+				} else {
+					lblLoginErrado.setVisible(true);
+					t.schedule(5000);
+				}
+			}
 		}
-		
+
 	};
 	public static void setView(LoginGWT loginGWT) {
 		// TODO Auto-generated method stub
--- src/com/rhuan/client/cadastro/CadastroUsuarios.java
+++ src/com/rhuan/client/cadastro/CadastroUsuarios.java
@@ -3,10 +3,13 @@ package com.rhuan.client.cadastro;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.MouseDownEvent;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
@@ -25,19 +28,34 @@ public class CadastroUsuarios {
 		
 	}
 	public CadastroUsuarios() {
-		 Label lblLogin = new Label("Usuario");
-		 Label lblEmail = new Label("Email");
-		 Label lblSenha = new Label("Senha");
+		 Label lblLogin = new Label("Usuario:");
+		 Label lblEmail = new Label("Email:");
+		 Label lblSenha = new Label("Senha:");
+		 Label lblLoginBranco = new Label("Preencha todos os campos!");
+		 Label lblCadastro = new Label("Cadastrado com sucesso!");
+			Timer t = new Timer() {
+			      @Override
+			      public void run() {
+			    	  lblLoginBranco.setVisible(false);
+			    	  lblCadastro.setVisible(false);
+			      }
+			};
+		 
+			final  DialogBox dbPanel2= new DialogBox(){;
+		    protected void beginDragging(MouseDownEvent event) {
+		        event.preventDefault();
+		    };
+		};
 		
 		 TextBox txtLogin = new TextBox();
 		 TextBox txtEmail = new TextBox();
 		 PasswordTextBox txtSenha = new PasswordTextBox();
 		
 		Button btnVoltar =new Button("Voltar");
-		Button btnSalvar =new Button("Salvar");
-		
+		Button btnSalvar =new Button("Enviar");
+		FlexTable flTable = new FlexTable();
 		VerticalPanel vPanel = new VerticalPanel();
-		DialogBox dbPanel2 = new DialogBox();
+		
 		
 			
 			vPanel.add(lblLogin);
@@ -46,9 +64,21 @@ public class CadastroUsuarios {
 			vPanel.add(txtSenha);
 			vPanel.add(lblEmail);
 			vPanel.add(txtEmail);
-			vPanel.add(btnVoltar);
-			vPanel.add(btnSalvar);
+			vPanel.add(lblLoginBranco);
+			vPanel.add(lblCadastro);
+			lblLoginBranco.setStyleName("erro");
+			lblCadastro.setStyleName("correto");
+			flTable.setWidget(0, 0, btnVoltar);
+			flTable.setWidget(0, 8, btnSalvar);
+			flTable.setCellPadding(5);
+			vPanel.add(flTable);
 			
+			lblLoginBranco.setVisible(false);
+			lblCadastro.setVisible(false);
+			
+			txtLogin.getElement().setPropertyString("placeholder", "Digite um nome de usuario");
+			txtSenha.getElement().setPropertyString("placeholder", "Digite uma senha");
+			txtEmail.getElement().setPropertyString("placeholder", "Digite um email");
 			btnVoltar.addClickHandler(new ClickHandler() {
 				
 				@Override
@@ -64,16 +94,25 @@ public class CadastroUsuarios {
 				
 				@Override
 				public void onClick(ClickEvent event) {
+					if(!txtLogin.getText().isEmpty() && !txtSenha.getText().isEmpty() && !txtEmail.getText().isEmpty()) {
 					getServico().saveCadastro(txtLogin.getText(), txtSenha.getText(), txtEmail.getText(), callback);
-					
+					lblCadastro.setVisible(true);
+					t.schedule(5000);
+					} else {
+						lblLoginBranco.setVisible(true);
+						t.schedule(5000);
+					}
 				}
 			});
 			dbPanel2.add(vPanel);
-			dbPanel2.setText("Digite usuario e senha: ");
+			
+			
 			dbPanel2.setAnimationEnabled(true);
 			
+			dbPanel2.center();
 			
 			RootPanel.get().add(dbPanel2);
+			dbPanel2.center();
 	}
 	AsyncCallback<String> callback = new AsyncCallback<String>() {
 
@@ -86,9 +125,8 @@ public class CadastroUsuarios {
 		@Override
 		public void onSuccess(String result) {
 			if(result.equals("gravou")) {
-			Window.alert("cadastrado!");
-				
-			RootPanel.get().add((IsWidget) new LoginGWT());
+			RootPanel.get().clear();	
+			LoginGWT.setView( new LoginGWT());
 			}
 			else
 				Window.alert("Erro ao cadastrar");
